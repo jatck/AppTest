c3e25a7850bf7b6365f33e3f396a814b9bb05547
DESKTOP-URCM5RQ\JACZAW <zawislak.jacek@gmail.com>
2019-05-29 00:48:54 +0200

    Second commit

--- app/src/main/java/pl/jz/android/mojpierwszypojekt/ItemListActivity.java
+++ app/src/main/java/pl/jz/android/mojpierwszypojekt/ItemListActivity.java
@@ -47,7 +47,7 @@ public class ItemListActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Zastąp twoja akcją", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
